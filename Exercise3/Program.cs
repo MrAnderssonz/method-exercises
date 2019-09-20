62ef509bb35c6723f8b37754aec2371233b53a33
Johan Andersson <ewan.andersson@gmail.com>
2019-09-20 15:29:55 +0200

    Övning 3 klar!

---
+++ Exercise3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static int Addera(int input1, int input2)
+        {
+            int sum = input1 + input2;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tja");
+            Console.Write("Skriv in ett tal: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Skriv in ett andra tal: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Summan av dina tal {0} och {1} är {2}",input1, input2, Addera(input1, input2));
+            
+        }
+    }
+}
