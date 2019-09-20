544706f43bcba95e3030ee9e990ff353b944678d
Johan Andersson <ewan.andersson@gmail.com>
2019-09-20 15:20:22 +0200

    Övning 2 klar!

---
+++ Exercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static string MakeTwoIntoOne (string one, string two)
+        {
+            string TwoInOne = one + " " + two;
+            return TwoInOne;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello vän!");
+            Console.Write("Skriv något kul: ");
+            string input1 = Console.ReadLine();
+            Console.Write("Skriv något tråkigt ");
+            string input2 = Console.ReadLine();
+
+           Console.WriteLine(MakeTwoIntoOne(input1, input2));
+
+        }
+    }
+}
