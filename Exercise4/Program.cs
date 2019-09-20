f730310ded945e8d8440d7a12f557ac1e47b86f3
Johan Andersson <ewan.andersson@gmail.com>
2019-09-20 15:41:11 +0200

    Övning 4 klar!

---
+++ Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static double AddVat(double input)
+        {
+            double withVat = input * 1.25;
+            return withVat;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Vad är priset utan moms? ");
+            double withoutVat = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Då blir priset med moms: {0}", AddVat(withoutVat));
+
+        }
+    }
+}
