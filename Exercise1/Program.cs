f14d370499d8ad46c7bcb3cfb8873b0a38eafa27
Johan Andersson <ewan.andersson@gmail.com>
2019-09-20 15:06:30 +0200

    Övning 1 klar!

---
+++ Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static string Hello(string name)
+        {
+            Console.WriteLine("Hej {0}",name);
+            return name;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Vad är ditt namn? ");
+            string name = Console.ReadLine();
+            Hello(name);
+        }
+    }
+}
