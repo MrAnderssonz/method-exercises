e116f3b0b8493bd822bd742e788fbbafbe15116d
Johan Andersson <ewan.andersson@gmail.com>
2019-09-22 15:27:06 +0200

    Övning 5 klar!

---
+++ Exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static double AddVat(double input, int inputVat)
+        {
+            double percent = 100;
+            double vat = inputVat/percent + 1;
+            double withVat = input * vat;
+            return withVat;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Vad är priset utan moms? ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Hur många % ligger momesen på? ");
+            int vat = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}kr med {1}% moms, kostar varan {2}kr",price, vat, AddVat(price, vat));
+
+        }
+    }
+}
