f7e1a20b5835f68b53fe7d3b29281e3cedea0618
Johan Andersson <ewan.andersson@gmail.com>
2019-09-23 20:54:33 +0200

    Övning 6 klar!

---
+++ Exercise6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static int ToPercentage(double input)
+        {
+
+            input= Math.Round(input, 0);
+            int output = (int)input;
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ett decimaltal tack! ");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ditt decimaltal {0}, blir {1} som heltal.",userInput, ToPercentage(userInput));
+            Console.WriteLine("Tack o bock!");
+        }
+    }
+}
